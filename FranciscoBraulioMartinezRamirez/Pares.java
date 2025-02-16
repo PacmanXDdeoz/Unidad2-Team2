516edc8c79fa48ff6fb31ad6b7a1fc85ccecab4d
Franckbmr <Blizzard_mr@outlook.es>
2025-02-16 15:05:54 -0600

    1

---
+++ FranciscoBraulioMartinezRamirez/Pares.java
@@ -0,0 +1,8 @@
+class Pares{ //Iniciamos la clase
+ public static void main(String[] args){ // Método principal donde inicia la ejecución del programa
+  System.out.println("Numeros pares:\n"); // Titulo de lo que haremos
+   for ( int i = 0; i <= 1000; i += 2){ // Bucle donde se valua la condicion y se hace un incremento de 2 en 2.
+    System.out.println(i); // Impresion de los numeros
+     } // Cerramos bucle
+   } // Cerramos metodo
+} // Cerramos clase
