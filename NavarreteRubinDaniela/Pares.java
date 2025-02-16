8b363c59fe51f04add3a8392dbea1c7369fe6090
Daniela Navarrete <daniela.navarreteru@gmail.com>
2025-02-16 13:39:45 -0600

    Ejercicios

--- NavarreteRubinDaniela/Pares.java
+++ NavarreteRubinDaniela/Pares.java
@@ -1,7 +1,7 @@
 class Pares{ // Se define la clase
 public static void main(String[] args){ //Se define el metodo principal
  System.out.println("Numeros pares del 1 al 1000"); 
-for(int i = 0; i<=1000; i+=2){ //Utilizacion del for para iterar los numeros pares del 0 al 1000
+for(int i = 0; i<=1000; i+=2){ //Utilizacion del for para iterar los numeros del 0 al 1000, se utiliza la condicion i+=2 para que se itere cada 2 numeros
 System.out.println(i); // Se imprime 
 }
 }
