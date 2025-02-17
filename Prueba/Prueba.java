4d3a5eb4efee875f8c8ba1113e1619d3cb76ebb2
Carlos Vazquez <carlosangel2710@hotmail.com>
2025-02-17 13:37:11 -0600

    Prueba para Evelyn

---
+++ Prueba/Prueba.java
@@ -0,0 +1,12 @@
+class Multi1a9 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++){
+            System.out.println("Tabla de multiplicacion de " + i);
+            for (int j = 0; j <= 10; j++){
+                System.out.println(i + "*" + j + " = " + i * j);
+            }
+        }
+    }
+}
+
+//Esta es una prueba, no le hagan caso
